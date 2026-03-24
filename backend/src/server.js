e5bc26f7780d97bb53662cb2d8815d107a91a44a
Shashwat Singh <shashwat.singh@tothenew.com>
2026-03-24 13:43:22 +0530

    whitelist frontend url

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin : process.env.CLIENT_URL,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRoutes);
